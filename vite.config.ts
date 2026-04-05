8efcc1aa131286cce3842f64da68710ed0e8e116
Nyoman Sunima <nyomansunima@gmail.com>
2026-04-05 14:16:03 +0800

    chore: use the default paths resolver

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,23 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     port: 3000,
   },
   plugins: [
-    tsconfigPaths(),
     tailwindcss(),
     tanstackStart({
+      srcDirectory: "src/app",
       prerender: {
         enabled: false,
         autoSubfolderIndex: true,
         crawlLinks: true,
       },
-      srcDirectory: "src/app",
       sitemap: {
         enabled: true,
         host: "https://www.thecoined.site",
